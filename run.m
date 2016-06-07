97e120a138e76b667081c98d6c699102e3cf704b
Maximilian J. Zangs <maximilian.zangs@gmail.com>
2016-06-07 17:33:24 +0100

    More flexibility added

--- run.m
+++ run.m
@@ -7,14 +7,24 @@ saveName = 'subject_name';
 
 % Set this to true if the screen test does not work
 skipSync = true;
+% Determines whether to skip the square alignment test
+skipSquareTest = false;
+% The number of trials per block
+trialCount = 10;
+% The number of blocks that will be run and saved
+blockCount = 8;
 
 % Initialises the study
-catStudy = CatStudy(skipSync);
+catStudy = CatStudy(skipSync, skipSquareTest);
+% catStudy.egg('secret');
 
-% Runs through the trials one block at a time
-catStudy = catStudy.runTrials();
-% Saves the block's data (for all trials) in the output file
-catStudy.saveTrial(saveName);
+% Runs through the blocks
+for blockNumber = 1:blockCount
+    % Runs through the trials one block at a time
+    catStudy = catStudy.runTrials(trialCount);
+    % Saves the block's data (for all trials) in the output file
+    catStudy.saveTrial([saveName '_block' num2str(blockNumber)]);
+end
 
 % Closes all screens and does some tidyig (if required)
 catStudy.tidyUp();
